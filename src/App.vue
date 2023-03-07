41cc07e87df961b999d80ba406d8f8500a42bc4b
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-03-07 12:34:36 +0100

    day 3, step 2 and 3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
+<script setup>
+import NavLinks from './components/NavLinks.vue'
+</script>
+
 <template>
-  <nav>
-    <router-link to="/counter">Counter</router-link>
-    |
-    <router-link to="/counter-options">Counter Options</router-link>
-    |
-    <router-link to="/playground-components">Playground</router-link>
-    |
-    <router-link to="/todo-utility">Todo Utility</router-link>
-  </nav>
+  <NavLinks />
   <router-view />
 </template>
