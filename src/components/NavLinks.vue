13c07c6f0951c43ce438359bd6eba95e0142e2a1
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-03-07 13:09:56 +0100

    day 3, step 7

--- src/components/NavLinks.vue
+++ src/components/NavLinks.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useTodoStore } from '@/stores/todo'
+
+const todoStore = useTodoStore()
+const { todoItems } = storeToRefs(todoStore)
+</script>
+
 <template>
   <nav>
     <router-link :to="{ name: 'HomePage' }">Home</router-link>
@@ -8,6 +16,11 @@
     |
     <router-link :to="{ name: 'PlaygroundComponents' }">Playground</router-link>
     |
-    <router-link :to="{ name: 'TodoUtility' }">Todo Utility</router-link>
+    <router-link :to="{ name: 'TodoUtility' }">
+      Todo Utility
+      <span v-if="todoItems.length > 0">
+        ({{ todoItems.length }})
+      </span>
+    </router-link>
   </nav>
 </template>
