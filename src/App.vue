f82c6b2155ef5963a9c1466b0fefdd464b0087b2
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-03-07 09:33:04 +0100

    day 2, step 1 and 2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import TodoUtility from '@/components/todo/TodoUtility.vue'
 import Counter from '@/components/Counter.vue'
 import CounterOptions from '@/components/CounterOptions.vue'
 import PlaygroundComponents from '@/components/playground/Components.vue'
 </script>
 
 <template>
+  <TodoUtility />
   <Counter />
   <CounterOptions />
   <PlaygroundComponents />
---
+++ src/components/todo/TodoUtility.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const items = ref([])
+
+onMounted(() => {
+  items.value = [
+    { id: 1, text: 'farina', completed: false },
+    { id: 2, text: 'olio', completed: true },
+    { id: 3, text: 'pane', completed: false },
+    { id: 4, text: 'zucchero', completed: true },
+    { id: 5, text: 'sale', completed: false },
+  ]
+})
+</script>
+
+<template>
+  <h2>To-do Utility</h2>
+</template>
