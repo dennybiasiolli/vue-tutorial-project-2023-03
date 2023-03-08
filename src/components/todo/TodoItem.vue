3b689ce96d068537e81745b33ace41b0c01da184
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-03-08 13:03:11 +0100

    day 4, step 6.1

--- src/components/todo/TodoItem.vue
+++ src/components/todo/TodoItem.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref, watch } from 'vue'
+
 const props = defineProps({
   text: {
     type: String,
@@ -9,13 +11,41 @@ const props = defineProps({
     required: true,
   },
 })
-const emit = defineEmits(['todoChange'])
+const emit = defineEmits(['todoChange', 'textChanged'])
+
+const showForm = ref(false)
+const formValid = ref(false)
+const innerText = ref('')
+function handleEditClick() {
+  innerText.value = props.text
+  showForm.value = true
+}
+function handleCancelClick() {
+  showForm.value = false
+}
+function handleSaveClick() {
+  emit('textChanged', innerText.value)
+  showForm.value = false
+}
+function validateForm() {
+  formValid.value = !!innerText.value.trim()
+}
+watch(innerText, validateForm)
 </script>
 
 <template>
-  <input
-    type="checkbox"
-    :checked="completed"
-    @change="emit('todoChange', $event.target.checked)"
-  /> {{ text }}
+  <span v-if="!showForm">
+    <input
+      type="checkbox"
+      :checked="completed"
+      @change="emit('todoChange', $event.target.checked)"
+    />
+    {{ text }}
+    <button @click="handleEditClick">Edit</button>
+  </span>
+  <span v-else>
+    <input type="text" v-model="innerText" />
+    <button type="button" @click="handleCancelClick">Cancel</button>
+    <button type="button" :disabled="!formValid" @click="handleSaveClick">Save</button>
+  </span>
 </template>
