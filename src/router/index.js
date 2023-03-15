d5368933bc3c3e8e7aefa6c57c892c80cb3f5495
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-03-15 10:40:02 +0100

    router: using lazy import for todo route

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import HomePage from '@/components/HomePage.vue'
 import Inputs from '@/components/Inputs.vue'
 import NotFound from '@/components/NotFound.vue'
 import PlaygroundComponents from '@/components/playground/Components.vue'
-import TodoUtility from '@/components/todo/TodoUtility.vue'
+// import TodoUtility from '@/components/todo/TodoUtility.vue'
 import Watchers from '@/components/Watchers.vue'
 
 export const routes = [
@@ -58,7 +58,7 @@ export const routes = [
   {
     name: 'TodoUtility',
     path: '/todo-utility',
-    component: TodoUtility,
+    component: () => import('@/components/todo/TodoUtility.vue'),
   },
   {
     name: 'NotFound',
